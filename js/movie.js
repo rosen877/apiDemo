b3e5e7e9f96b7c9c0cf146afcd01eaaaf6d6e337
rosen877 <jnmmyli@yahoo.com.tw>
2018-12-10 22:49:50 +0800

    增加搜尋跟更新觀看人數

--- js/movie.js
+++ js/movie.js
@@ -2,45 +2,36 @@ $(function () {
 	var cont=0;
 	$.ajax({
 		method: "get",
-		url:"https://api.themoviedb.org/3/trending/movie/week?api_key=71660fbe32d785dee5f86fddf9af16f9",
+		url:"https://api.themoviedb.org/3/movie/popular?api_key=71660fbe32d785dee5f86fddf9af16f9&language=zh-tw&page=1",
 	}).done(function( msg ) {
 		console.log(msg);
-		var movieRow = [];
-		
-		var num = msg.results.length;
-		var Hotresults = msg.results;
-		
-		var data = msg.results[0].release_date;
-		var description = msg.results[0].overview;
+		let num = msg.results.length;
+		let Hotresults = msg.results;
+		let data = msg.results[0].release_date;
 		Hotresults.forEach((movie) => {
-
 			var id =[];
 			id.push(movie.id);
-
-
 			let shell = document.createElement('div');
 			$('.output').append(shell);
-
-			
 			shell.classList.add('movieWrap');
-
 			let url = "https://image.tmdb.org/t/p/w185_and_h278_bestv2/"+ movie.poster_path;
 
-			shell.innerHTML = '<div class="picWarp data">'+
-			'<img src="'+url+'">'+
-			'</div>'+
-			'<div class="movieWrap__content">'+
-				'<div class="titile">'+
-						'<h2 class="movieName">'+movie.original_title+'</h2>'+
-						'<p class="date">上映日期：'+data+'</p>'+
-				'</div>'+
-
+			shell.innerHTML = '<div class="picWarp">'+
+		'	<img src="'+url+'">'+
+		'	<div class="videoWarp__content">'+
+			'  <div class="titile">'+
+				'<h2 class="movieName">'+movie.original_title+'</h2>'+
+				'<p class="date">上映日期：'+data+'</p>'+
 				'<div class="description">'+
-					'<h3>簡介</h3>'+
-					'<p class="description__content">'+description+'</p>'+'<div class="view">官網資訊</div>'+
-'<input class="movieId" type="hidden" value="'+movie.id+'">'+
+				'  <h3>簡介</h3>'+
+				 ' <p class="description__content">'+movie.overview+'</p>'+
+				 ' <div class="view">官網資訊</div>'+
+				  '<input class="movieId" type="hidden" value="'+movie.id+'">'+
 				'</div>'+
-			'</div>';
+			'  </div>'+
+			'</div>'+
+		 ' </div>'+
+		'</div>';
 			$('.videoWarp').html(shell);
 	
 		});
@@ -54,6 +45,52 @@ $(function () {
 
 })
 
+function search(){
 
+}
+$(".searchWarp input").keydown(function (event) {
+	if (event.which == 13) {
+		
+		var searchContent = $(".searchWarp input").val();
+		$.ajax({
+			method: "get",
+			url:"https://api.themoviedb.org/3/search/movie?api_key=71660fbe32d785dee5f86fddf9af16f9&language=zh-tw&query="+searchContent+"&page=1&include_adult=false",
+		}).done(function( msg ) {
+			console.log(msg);
+			$(".output").children().remove();
+			let num = msg.results.length;
+			let Hotresults = msg.results;
+			let data = msg.results[0].release_date;
+			Hotresults.forEach((movie) => {
+				var id =[];
+				id.push(movie.id);
+				let shell = document.createElement('div');
+				$('.output').append(shell);
+				shell.classList.add('movieWrap');
+				let url = "https://image.tmdb.org/t/p/w185_and_h278_bestv2/"+ movie.poster_path;
 	
+				shell.innerHTML = '<div class="picWarp">'+
+			'	<img src="'+url+'">'+
+			'	<div class="videoWarp__content">'+
+				'  <div class="titile">'+
+					'<h2 class="movieName">'+movie.original_title+'</h2>'+
+					'<p class="date">上映日期：'+data+'</p>'+
+					'<div class="description">'+
+					'  <h3>簡介</h3>'+
+					 ' <p class="description__content">'+movie.overview+'</p>'+
+					 ' <div class="view">官網資訊</div>'+
+					  '<input class="movieId" type="hidden" value="'+movie.id+'">'+
+					'</div>'+
+				'  </div>'+
+				'</div>'+
+			 ' </div>'+
+			'</div>';
+				$('.videoWarp').html(shell);
+		
+			})
+			});
+
+			
+	}
+	})
 // https://image.tmdb.org/t/p/w185_and_h278_bestv2/
--- js/ytbTop.js
+++ js/ytbTop.js
@@ -29,92 +29,164 @@
 $(function(){
 
 
-var ids = ["UCLW_SzI9txZvtOFTPDswxqg","UCj_z-Zeqk8LfwVxx0MUdL-Q","UCPRWWKG0VkBA0Pqa4Jr5j0Q"];
+// var ids = ["UCLW_SzI9txZvtOFTPDswxqg","UCj_z-Zeqk8LfwVxx0MUdL-Q","UCPRWWKG0VkBA0Pqa4Jr5j0Q"];
 
-var count = 0;
-$('#next').click(function(){
-	count = count + 1;
-	if(count > 2 ){
-		count=0;
-	}
-	console.log(count);
-	updata(ids[count]);
-});
-$('#last').click(function(){
-	count = count - 1;
-	if(count < 0 ){
-		count=2;
-	}
-	console.log(count);
-	updata(ids[count]);
-});
+// var count = 0;
+//  $('#next').click(function(){
+// 		clearInterval(first);
+// 		if(lastInt){
+// 			clearInterval(lastInt);
+// 		}
+		
+// 		count = count + 1;
+// 		if(count > 2 ){
+// 			count=0;
+// 		}
+// 		console.log(count);
+// 		updata(ids[count]);
+// 		function nextInt(){
+// 			updata(ids[count]);
+// 		}
+// 		var nextInt = setInterval(nextInt, 1000);
+// 		console.log(nextInt);
+	
+	
+// });
+// $('#last').click(function(){
+// 	clearInterval(first);
+// 		count = count - 1;
+// 		if(count < 0 ){
+// 			count=2;
+// 		}
+// 		console.log(count);
+// 		updata(ids[count]);
+// 		function lastInt(){
+// 			updata(ids[count]);
+// 		}
+// 		var lastInt = setInterval(lastInt, 1000);
+	
+// });
 
 	function updata(id){
 		$.ajax({
 			method: "get",
 			url:"https://www.googleapis.com/youtube/v3/channels" +
-			"?id="+id+"" +
+			"?id=UCLW_SzI9txZvtOFTPDswxqg" +
 			"&key=AIzaSyCta64_vT9ZB43rGLB4Hhtf2hErNFojh0M" +
 			"&part=snippet,contentDetails,statistics",
 		})
 			.done(function( msg ) {
-			
+				console.log(msg);
 				let channelName = msg.items[0].snippet.title;
 				let trackNum = msg.items[0].statistics.subscriberCount;
 				let watchNum = msg.items[0].statistics.viewCount;
 				let videoNum = msg.items[0].statistics.videoCount;
 				let contentText = msg.items[0].snippet.description;
 				let pic = msg.items[0].snippet.thumbnails.medium.url;
-			
-	
-				$(".chennelName").text(channelName);
-				$(".trackNum").text(trackNum);
-				$(".watchNum").text(watchNum);
-				$(".videoNum").text(videoNum);
-				$(".contentText").text(contentText);
-				$(".chennelBg").css("background-image","url("+pic+")")
-			});
+				let textWidth = $(".chennelBg").width();
+				
 
-			$(".videoBtn").click(function (){
+				function setMoney(num){//取整，三行逗号隔开
+					return parseFloat(num).toLocaleString();
+					
+				}
+				let liveNum=setMoney(watchNum);
+				
+				$(".chennelName.one").text(channelName);
+				$(".watchNum.one").text(liveNum );
+				$(".chennelBg.one img").attr("src",pic);
+				
+			});
+			$.ajax({
+				method: "get",
+				url:"https://www.googleapis.com/youtube/v3/channels" +
+				"?id=UCj_z-Zeqk8LfwVxx0MUdL-Q" +
+				"&key=AIzaSyCta64_vT9ZB43rGLB4Hhtf2hErNFojh0M" +
+				"&part=snippet,contentDetails,statistics",
+			})
+				.done(function( obj ) {
+					console.log(obj);
+					let channelName2 = obj.items[0].snippet.title;
+					let watchNum2 = obj.items[0].statistics.viewCount;
+					let pic2 = obj.items[0].snippet.thumbnails.medium.url;
+					
+	
+					function setMoney(num){
+						return parseFloat(num).toLocaleString();
+						
+					}
+					let liveNumf=setMoney(watchNum2);
+					
+					$(".chennelName.two").text(channelName2);
+					$(".watchNum.two").text(liveNumf );
+					$(".chennelBg.two img").attr("src",pic2);
+				});
 				$.ajax({
 					method: "get",
-					url:"https://www.googleapis.com/youtube/v3/playlists" +
+					url:"https://www.googleapis.com/youtube/v3/channels" +
 					"?id=UCPRWWKG0VkBA0Pqa4Jr5j0Q" +
 					"&key=AIzaSyCta64_vT9ZB43rGLB4Hhtf2hErNFojh0M" +
 					"&part=snippet,contentDetails,statistics",
 				})
-					.done(function( msg ) {
-					
-						let channelName = msg.items[0].snippet.title;
-						let trackNum = msg.items[0].statistics.subscriberCount;
-						let watchNum = msg.items[0].statistics.viewCount;
-						let videoNum = msg.items[0].statistics.videoCount;
-						let contentText = msg.items[0].snippet.description;
-						let pic = msg.items[0].snippet.thumbnails.medium.url;
-					
-			
-						$(".chennelName").text(channelName);
-						$(".trackNum").text(trackNum);
-						$(".watchNum").text(watchNum);
-						$(".videoNum").text(videoNum);
-						$(".contentText").text(contentText);
-						$(".chennelBg").css("background-image","url("+pic+")")
+					.done(function( objtr ) {
+						console.log(objtr);
+						let channelNamejo = objtr.items[0].snippet.title;
+						let trackNum = objtr.items[0].statistics.subscriberCount;
+						let watchNum3 = objtr.items[0].statistics.viewCount;
+						let pic3 = objtr.items[0].snippet.thumbnails.medium.url;
+						
+		
+						function setMoney(num){//取整，三行逗号隔开
+							return parseFloat(num).toLocaleString();
+							
+						}
+						let liveNumg=setMoney(watchNum3);
+						
+						$(".chennelName.three").text(channelNamejo);
+						$(".watchNum.three").text(liveNumg );
+						$(".chennelBg.three img").attr("src",pic3);
 					});
-			})
-	}
 
-updata(ids[0]);
-
-		// $.ajax({
-		// 	method: "PUT",
-		// 	url:"https://www.googleapis.com/youtube/v3/channels" +
-		// 	"?id=UCPRWWKG0VkBA0Pqa4Jr5j0Q" +
-		// 	"&key=AIzaSyCta64_vT9ZB43rGLB4Hhtf2hErNFojh0M" +
-		// 	"&part=brandingSettings,statistics"+"&onBehalfOfContentOwner=''"+
-		// 	"",
-		// })
-		// 	.done(function( msg ) {
-		// 		console.log(msg);
+			// $(".videoBtn").click(function (){
+			// 	$.ajax({
+			// 		method: "get",
+			// 		url:"https://www.googleapis.com/youtube/v3/playlists" +
+			// 		"?id=UCPRWWKG0VkBA0Pqa4Jr5j0Q" +
+			// 		"&key=AIzaSyCta64_vT9ZB43rGLB4Hhtf2hErNFojh0M" +
+			// 		"&part=snippet,contentDetails,statistics",
+			// 	})
+			// 		.done(function( msg ) {
+					
+			// 			let channelName = msg.items[0].snippet.title;
+			// 			let trackNum = msg.items[0].statistics.subscriberCount;
+			// 			let watchNum = msg.items[0].statistics.viewCount;
+			// 			let videoNum = msg.items[0].statistics.videoCount;
+			// 			let contentText = msg.items[0].snippet.description;
+			// 			let pic = msg.items[0].snippet.thumbnails.medium.url;
+					
 			
-		// 	});
+			// 			$(".chennelName").text(channelName);
+			// 			$(".trackNum").text(trackNum);
+			// 			$(".watchNum").text(watchNum);
+			// 			$(".videoNum").text(videoNum);
+			// 			$(".contentText").text(contentText);
+			// 			$(".chennelBg").css("background-image","url("+pic+")")
+						
+			// 		});
+			// })
+			
+	}
+	updata();
+	setInterval(updata, 1000);
+
+		var left = 50;
+		$('.channelwarp').click(function(){
+			
+			left += 420 *-1;
+			if(left < -790){
+				left = 50;
+			}
+			console.log(left);
+			$('.containerWapr').css("left",left);
+		})
 })
